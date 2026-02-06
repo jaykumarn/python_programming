2611207c30d648801b530860ed5418b409890612
jaykumarn <jaykumar.nimbalkar@gmail.com>
2026-02-06 14:02:30 +0530

    choose maximum

--- maximum_number.py
+++ maximum_number.py
@@ -2,7 +2,9 @@ x = 34
 
 y = 50
 
-if x > y
-print "x is greater"
-else
-print "y is greater"
+if x > y:
+    print("x is greater")
+elif x < y:
+    print("y is greater")
+else:
+    print("x and y are equal")
